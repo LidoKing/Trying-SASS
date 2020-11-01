62265f96f8182cbcc2d5ab563a8c3abc9f43d2a2
LidoKing <kinglido203@gmail.com>
2020-11-01 17:59:48 +0800

    Added local storage to keep theme after reload

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const dark = document.getElementById('dark');
 
 //change class of body tag on click
 light.onclick = () => {
-  body.classList.replace('dark', 'light')
+  body.classList.replace('dark', 'light');
+  localStorage.setItem('theme', 'light');
 };
 
 dark.onclick = () => {
   body.classList.replace('light','dark')
+  localStorage.setItem('theme', 'dark');
 };
+
+//local storage, apply cached theme on reload
+const theme = localStorage.getItem('theme');
+
+if (theme) {
+  body.classList.add(theme);
+}
