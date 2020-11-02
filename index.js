6bf0a754e23d1510a98dad207123f2bc46767ddd
LidoKing <kinglido203@gmail.com>
2020-11-02 16:13:35 +0800

    project done

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 //DOM elements
 const body = document.body;
-const light = document.getElementById('light');
-const dark = document.getElementById('dark');
+const changeTheme = document.getElementById('toggle');
 
 //change class of body tag on click
-light.onclick = () => {
-  body.classList.replace('dark', 'light');
-  localStorage.setItem('theme', 'light');
-};
+changeTheme.onclick = () => {
+  if (body.classList == 'light') {
+    body.classList.replace('light','dark');
 
-dark.onclick = () => {
-  body.classList.replace('light','dark')
-  localStorage.setItem('theme', 'dark');
-};
+    localStorage.setItem('theme', 'dark');
+  } else {
+    body.classList.replace('dark','light');
+    localStorage.setItem('theme', 'light');
+
+  }
+}
 
 //local storage, apply cached theme on reload
 const theme = localStorage.getItem('theme');
